a20396cd557f02850328a9f6a86a7a24610e64d5
ryusu12 <rsy7567@gmail.com>
2025-05-28 17:30:33 +0900

    fix: 불필요한 어노테이션 삭제 및 조건 변경
    Pattern으로 이미 null과 공백을 처리중
    - NotEmpty 불필요함에 따라 삭제
    
    userName 최소 2글자 이상으로 조건 변경

--- src/main/java/com/example/newsfeed/User/controller/dto/UpdateUserPasswordReqDto.java
+++ src/main/java/com/example/newsfeed/User/controller/dto/UpdateUserPasswordReqDto.java
@@ -1,18 +1,15 @@
 package com.example.newsfeed.User.controller.dto;
 
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 
 @Getter
 public class UpdateUserPasswordReqDto {
 
-    @NotEmpty()
     @Pattern(regexp = "^.*(?=^.{8,20}$)(?=.*\\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$",
             message = "비밀번호는 영문자, 숫자, 특수문자가 최소 1글자씩 있어야합니다. 길이는 8 ~ 20 입니다.")
     private final String password;
 
-    @NotEmpty()
     @Pattern(regexp = "^.*(?=^.{8,20}$)(?=.*\\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$",
             message = "비밀번호는 영문자, 숫자, 특수문자가 최소 1글자씩 있어야합니다. 길이는 8 ~ 20 입니다.")
     private final String newPassword;
--- src/main/java/com/example/newsfeed/User/controller/dto/UpdateUserReqDto.java
+++ src/main/java/com/example/newsfeed/User/controller/dto/UpdateUserReqDto.java
@@ -1,18 +1,17 @@
 package com.example.newsfeed.User.controller.dto;
 
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 
 @Getter
 public class UpdateUserReqDto {
 
-    @NotEmpty()
     @Pattern(regexp = "^.*(?=^.{8,20}$)(?=.*\\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$",
             message = "비밀번호는 영문자, 숫자, 특수문자가 최소 1글자씩 있어야합니다. 길이는 8 ~ 20 입니다.")
     private final String password;
 
-    @Pattern(regexp = "^[a-zA-Z0-9가-힣]{0,10}$", message = "이름은 영문자, 한글, 숫자만 사용가능하며, 길이는 10 이하 입니다.")
+    @Pattern(regexp = "^[a-zA-Z0-9가-힣]{2,10}$",
+            message = "이름은 영문자, 한글, 숫자만 사용가능하며, 길이는 2 ~ 10 입니다.")
     private final String userName;
 
     private final String info;
