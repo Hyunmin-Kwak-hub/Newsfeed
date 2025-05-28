2f2be0faae7463b027957d73cab1ae644435f137
ryusu12 <rsy7567@gmail.com>
2025-05-28 14:52:08 +0900

    feat: 비밀번호 길이 최소 조건 8로 변경
    요구사항에서 비밀번호가 최소 8글자를 요구함
    - 모든 ReqDto에서 비밀번호 길이 조건 수정: 4~20 -> 8~20

--- src/main/java/com/example/newsfeed/User/controller/dto/DeleteUserReqDto.java
+++ src/main/java/com/example/newsfeed/User/controller/dto/DeleteUserReqDto.java
@@ -8,8 +8,8 @@ import lombok.Getter;
 public class DeleteUserReqDto {
 
     @NotEmpty()
-    @Pattern(regexp = "^.*(?=^.{4,20}$)(?=.*\\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$",
-            message = "비밀번호는 영문자, 숫자, 특수문자만 사용가능하며, 최소 각각 1개씩은 있어야합니다. 길이는 4 ~ 20 입니다.")
+    @Pattern(regexp = "^.*(?=^.{8,20}$)(?=.*\\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$",
+            message = "비밀번호는 영문자, 숫자, 특수문자가 최소 1글자씩 있어야합니다. 길이는 8 ~ 20 입니다.")
     private final String password;
 
     public DeleteUserReqDto(String password) {
--- src/main/java/com/example/newsfeed/User/controller/dto/UpdateUserReqDto.java
+++ src/main/java/com/example/newsfeed/User/controller/dto/UpdateUserReqDto.java
@@ -8,16 +8,16 @@ import lombok.Getter;
 public class UpdateUserReqDto {
 
     @NotEmpty()
-    @Pattern(regexp = "^.*(?=^.{4,20}$)(?=.*\\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$",
-            message = "비밀번호는 영문자, 숫자, 특수문자만 사용가능하며, 최소 각각 1개씩은 있어야합니다. 길이는 4 ~ 20 입니다.")
+    @Pattern(regexp = "^.*(?=^.{8,20}$)(?=.*\\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$",
+            message = "비밀번호는 영문자, 숫자, 특수문자가 최소 1글자씩 있어야합니다. 길이는 8 ~ 20 입니다.")
     private final String password;
 
-    @Pattern(regexp = "^.*(?=^.{4,20}$)(?=.*\\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$",
-            message = "비밀번호는 영문자, 숫자, 특수문자만 사용가능하며, 최소 각각 1개씩은 있어야합니다. 길이는 4 ~ 20 입니다.")
+    @Pattern(regexp = "^.*(?=^.{8,20}$)(?=.*\\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$",
+            message = "비밀번호는 영문자, 숫자, 특수문자가 최소 1글자씩 있어야합니다. 길이는 8 ~ 20 입니다.")
     private final String newPassword;
 
     // 회원정보 수정 : 이름, 비밀번호, 소개글, 프로필 사진
-    @Pattern(regexp = "^[a-zA-Z0-9가-힣]{1,10}$", message = "이름은 영문자, 한글, 숫자만 사용가능하며, 길이는 1 이상 10 이하 입니다.")
+    @Pattern(regexp = "^[a-zA-Z0-9가-힣]{0,10}$", message = "이름은 영문자, 한글, 숫자만 사용가능하며, 길이는 10 이하 입니다.")
     private final String userName;
 
     private final String info;
