2a14f70b36594908082a11fc092bcca3c6b28fe8
hyunmin <khmin052@naver.com>
2025-06-02 20:37:03 +0900

    feat: 팔로우 취소 기능 구현(DELETE)

--- src/main/java/com/example/newsfeed/follow/controller/FollowController.java
+++ src/main/java/com/example/newsfeed/follow/controller/FollowController.java
@@ -2,11 +2,15 @@ package com.example.newsfeed.follow.controller;
 
 import com.example.newsfeed.follow.controller.dto.FollowReqDto;
 import com.example.newsfeed.follow.controller.dto.FollowResDto;
+import com.example.newsfeed.follow.controller.dto.FollowedUserArticleDto;
+import com.example.newsfeed.follow.controller.dto.FollowedUserDto;
 import com.example.newsfeed.follow.service.FollowService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/{userId}/follows")
@@ -24,5 +28,25 @@ public class FollowController {
         FollowResDto resDto = followService.saveFollow(userId, followReqDto.getFollowedUserId());
 
         return new ResponseEntity<>(resDto, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<FollowedUserDto>> getFollowedUsers(@PathVariable Long userId) {
+        List<FollowedUserDto> followedUsers = followService.getFollowedUsers(userId);
+
+        return new ResponseEntity<>(followedUsers, HttpStatus.OK);
+    }
+
+    @GetMapping("/articles")
+    public List<FollowedUserArticleDto> getFollwedUsersArticles(@PathVariable Long userId) {
+        return followService.getFollowedUsersArticles(userId);
+    }
+
+    @DeleteMapping("/unfollows/{followedId}")
+    public ResponseEntity<String> unfollow(
+            @PathVariable("userId") Long userId,
+            @PathVariable("followedId") Long followedId) {
+        followService.unfollow(userId, followedId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
